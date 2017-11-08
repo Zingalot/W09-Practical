b5df9760825d721b564eea26575b7d493d40e6fc
Toby <tts2@st-andrews.ac.uk>
2017-11-08 16:47:52 +0000

    Finished board set up

--- src/Board.java
+++ src/Board.java
@@ -6,9 +6,19 @@ public class Board {
     private static final char INVALID = ' ';
     private static final char FOX     = '*';
     private static final char GOOSE   = 'o';
+    private static final int  MIN_INDEX = 0;
+    private static final int  MAX_INDEX = DEFAULT_SIZE-1;
+
 
     private int boardsize;
     private char[][] board;
+
+    private void invalid2x2Square(int x , int y) {
+        board[x][y] = INVALID;
+        board[x][y+1] = INVALID;
+        board[x+1][y] = INVALID;
+        board[x+1][y+1] = INVALID;
+    }
 
     // Default constructor was provided by the starter code. Extend as needed (kt54)
     public Board() {
@@ -21,9 +31,31 @@ public class Board {
             for(int y=0; y<boardsize; y++)
                 board[x][y] = FREE;
 
-        // Put a single fox in the middle
-        board[boardsize/2][boardsize/2] = FOX;
+        //Replace corners with invalid squares
+        invalid2x2Square(MIN_INDEX , MIN_INDEX);
+        invalid2x2Square(MIN_INDEX  , MAX_INDEX - 1 );
+        invalid2x2Square(MAX_INDEX - 1 , MIN_INDEX);
+        invalid2x2Square(MAX_INDEX - 1 , MAX_INDEX - 1);
+
+        // Put a single fox in the starting position
+        board[boardsize/2][(boardsize+3)/2] = FOX;
+
+        //Put geese in starting positions
+        for(int i = MIN_INDEX; i <= MAX_INDEX; i++){
+            for(int j = MIN_INDEX; j <= MAX_INDEX/2; j++){
+                if(board[i][j] == FREE) {
+                    board[i][j] = GOOSE;
+                    //not sure how to avoid "magic constants" here
+                    if(j == MAX_INDEX/2 && (i == 2 || i == 3 || i == 4)){
+                        board[i][j] = FREE;
+                    }
+
+                }
+            }
+        }
     }
+
+
 
     // Prints the board. This method was provided with the starter code. Better not modify to ensure
     // output consistent with the autochecker (kt54)
