c12b27b723eb5f48fe07d64d7e48fc99d680f51d
Toby <tts2@st-andrews.ac.uk>
2017-11-08 15:44:33 +0000

    Importing Starting Code

--- src/W09Practical.java
+++ src/W09Practical.java
@@ -1,6 +1,9 @@
 public class W09Practical {
-    public static void main(String[] args){
-        System.out.println("Hello");
-        System.out.println("does this work?");
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+
+        game.play();
     }
 }
