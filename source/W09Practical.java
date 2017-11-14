46fed5a649c5546f90d8c13c7ff59502fa2282fd
zingalot <tobytheodoresmith@gmail.com>
2017-11-14 11:14:02 +0000

    Up to 7/8 Tests working

---
+++ source/W09Practical.java
@@ -0,0 +1,14 @@
+public class W09Practical {
+
+    public static void main(String[] args) {
+
+        //Below should be the final code to run the game
+        //Commented out for testing
+        //Directories named for github
+       Game game = new Game();
+
+        game.play();
+
+    }
+
+}
