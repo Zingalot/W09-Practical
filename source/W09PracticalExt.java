593395c9b828a6e31c1e928f1108c73d0cfcc26d
zingalot <tobytheodoresmith@gmail.com>
2017-11-16 11:58:42 +0000

    All tests working!

---
+++ source/W09PracticalExt.java
@@ -0,0 +1,15 @@
+public class W09PracticalExt {
+
+    public static void main(String[] args) {
+
+        //Below should be the final code to run the game
+        //Commented out for testing
+        //Directories named for github
+        Game game = new Game();
+
+        game.play();
+
+
+    }
+
+}
